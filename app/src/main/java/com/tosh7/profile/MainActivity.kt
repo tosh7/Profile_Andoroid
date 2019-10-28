55a8480522c48c83e1248a8f25eba6d31c2022f7
tosh7 <zlia.6.lj.425@gmail.com>
2019-10-29 02:33:46 +0900

    advancedまで完成

--- app/src/main/java/com/tosh7/profile/MainActivity.kt
+++ app/src/main/java/com/tosh7/profile/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.tosh7.profile
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -11,12 +12,48 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        myImageView.visibility = View.INVISIBLE
-        myText.visibility = View.INVISIBLE
+//        my_image_view.visibility = View.INVISIBLE
+//        my_text.visibility = View.INVISIBLE
+//
+//        my_name_button.setOnClickListener {
+//            my_image_view.visibility = View.VISIBLE
+//            my_text.visibility = View.VISIBLE
+//        }
 
-        myNameButton.setOnClickListener {
-            myImageView.visibility = View.VISIBLE
-            myText.visibility = View.VISIBLE
+        my_name_button.setOnClickListener {
+            my_name_button.setBackgroundColor(Color.parseColor( "#29abe2"))
+            my_like_sports_button.setBackgroundColor(Color.parseColor("#929292"))
+            my_like_food_button.setBackgroundColor(Color.parseColor("#929292"))
+            my_like_hobby_button.setBackgroundColor(Color.parseColor("#929292"))
+            my_image_view.setImageResource(R.drawable.tobisuke)
+            my_text.text = "とびすけ"
+        }
+
+        my_like_sports_button.setOnClickListener {
+            my_name_button.setBackgroundColor(Color.parseColor( "#929292"))
+            my_like_sports_button.setBackgroundColor(Color.parseColor("#29abe2"))
+            my_like_food_button.setBackgroundColor(Color.parseColor("#929292"))
+            my_like_hobby_button.setBackgroundColor(Color.parseColor("#929292"))
+            my_image_view.setImageResource(R.drawable.soccer)
+            my_text.text = "サッカー"
+        }
+
+        my_like_food_button.setOnClickListener {
+            my_name_button.setBackgroundColor(Color.parseColor( "#929292"))
+            my_like_sports_button.setBackgroundColor(Color.parseColor("#929292"))
+            my_like_food_button.setBackgroundColor(Color.parseColor("#29abe2"))
+            my_like_hobby_button.setBackgroundColor(Color.parseColor("#929292"))
+            my_image_view.setImageResource(R.drawable.sushi)
+            my_text.text = "寿司"
+        }
+
+       my_like_hobby_button.setOnClickListener {
+            my_name_button.setBackgroundColor(Color.parseColor( "#929292"))
+            my_like_sports_button.setBackgroundColor(Color.parseColor("#929292"))
+            my_like_food_button.setBackgroundColor(Color.parseColor("#929292"))
+            my_like_hobby_button.setBackgroundColor(Color.parseColor("#29abe2"))
+            my_image_view.setImageResource(R.drawable.dance)
+            my_text.text = "ダンス"
         }
     }
 }
